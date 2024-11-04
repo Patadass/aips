fb5f11daec3601065084bad38ba0bbf62a89fe1e
Patadas <54476077+Patadass@users.noreply.github.com>
2024-11-04 22:47:59 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        int[] a = {5,4,2,1,9,0,2};
-        new Sort.MergeSort(a);
-        for(int i = 0;i < a.length;i++){
-            System.out.print(a[i] + " ");
-        }
     }
 }
